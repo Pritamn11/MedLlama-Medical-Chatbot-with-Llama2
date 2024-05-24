183b1c3b1923dc0db0bdc5bd75e4ec8467b7aa55
Pritamn11 <pritamnarwade11@gmail.com>
2024-05-24 19:02:12 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name='GenAI medical chatbot',
+    version='0.0.0',
+    author='Pritam Narwade',
+    author_email='pritamnarwade11@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
